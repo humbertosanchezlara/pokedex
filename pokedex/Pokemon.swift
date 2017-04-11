a63029ee0a5c536cce92f89112b50a95ec8abb86
Humberto Sanchez Lara <humbertoslara@gmail.com>
2017-04-11 13:01:43 -0500

    Initial commit

---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,32 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Humberto Sanchez Lara on 1/12/17.
+//  Copyright © 2017 Humberto Sanchez. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        
+        self._name = name
+        self._pokedexId = pokedexId
+        
+    }
+    
+    
+}
