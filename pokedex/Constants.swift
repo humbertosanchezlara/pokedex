3b3689918d988ae0d6623aec8524852747b79f22
Humberto Sanchez Lara <humbertoslara@gmail.com>
2017-04-21 17:02:39 -0500

    Pokedex implementation

---
+++ pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Humberto Sanchez Lara on 4/20/17.
+//  Copyright © 2017 Humberto Sanchez. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
